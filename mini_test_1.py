58a8af9885555d52e082751f9950a2cef3ee238d
airellzulkarnain <airell.zulkarnain@gmail.com>
2023-08-15 11:01:30 +0700

    correction

--- mini_test_1.py
+++ mini_test_1.py
@@ -1,23 +1,19 @@
 arr = [i+1 for i in range(100)]
-
-def mutate_arr(n: int) -> int|str:
-    if n % 3 == 0 and n % 5 == 0:
-        return "FooBar"
-    elif n % 3 == 0:
-        return "Foo"
-    elif n % 5 == 0:
-        return "Bar"
-    else:
-        return n
     
-arr = [i for i in map(mutate_arr, arr)]
-for i in range(len(arr), 0, -1):
+for i in arr[::-1]:
         is_prime = True
         for num in range(2, i):
             if (i % num) == 0:
                 is_prime = False
                 break
         if i == 1:
-            print(arr[i-1], end=", ")
+            print(i, end=", ")
         elif not is_prime:
-            print(arr[i-1], end=", ")
+            if i % 3 == 0 and i % 5 == 0: 
+                 print("FooBar", end=", ")
+            elif i % 5 == 0 :
+                 print("Bar", end=", ")
+            elif i % 3 == 0 :
+                 print("Foo", end=", ")
+            else: 
+                print(i, end=", ")
